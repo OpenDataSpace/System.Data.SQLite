90df9e590d872feb0da409152ad849f1fdb8dec0
rmsimpson <rmsimpson>
2005-08-04 15:31:01 +0000

    1.0.12 updates

--- SQLite.Interop/AssemblyInfo.cpp
+++ SQLite.Interop/AssemblyInfo.cpp
@@ -27,7 +27,7 @@ using namespace System::Runtime::InteropServices;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("1.0.11.*")];
+[assembly:AssemblyVersionAttribute("1.0.12.*")];
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
