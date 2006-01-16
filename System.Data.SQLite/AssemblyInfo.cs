bd1542c1d42b2200fc5742972f832a12415832d2
rmsimpson <rmsimpson>
2006-01-16 15:49:38 +0000

    1.0.24.4

--- System.Data.SQLite/AssemblyInfo.cs
+++ System.Data.SQLite/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Runtime.ConstrainedExecution;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.24.3")]
+[assembly: AssemblyVersion("1.0.24.4")]
 #if !PLATFORM_COMPACTFRAMEWORK
-[assembly: AssemblyFileVersion("1.0.24.3")]
+[assembly: AssemblyFileVersion("1.0.24.4")]
 #endif
