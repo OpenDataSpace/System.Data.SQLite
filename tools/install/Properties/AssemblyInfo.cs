67948f6fe5b744382e601014734b14a8996a0b54
rmsimpson <rmsimpson>
2006-02-13 04:54:48 +0000

    no message

--- tools/install/Properties/AssemblyInfo.cs
+++ tools/install/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("install")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SQLite Installer")]
+[assembly: AssemblyDescription("SQLite ADO.NET 2.0 Design-Time Installer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Black Castle Software, LLC")]
-[assembly: AssemblyProduct("install")]
-[assembly: AssemblyCopyright("Copyright © Black Castle Software, LLC 2006")]
+[assembly: AssemblyCompany("http://sqlite.phxsoftware.com")]
+[assembly: AssemblyProduct("SQLite Installer")]
+[assembly: AssemblyCopyright("Public Domain")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a9f795ef-3653-4821-a5d4-11984be973fb")]
 
 // Version information for an assembly consists of the following four values:
 //
