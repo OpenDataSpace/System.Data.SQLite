800ccd2b1e06250968a1b23a37ab9f77dc087edc
rmsimpson <rmsimpson>
2007-06-19 22:42:31 +0000

    Code merge with SQLite 3.4.0

--- SQLite.Interop/src/sqlite3ext.h
+++ SQLite.Interop/src/sqlite3ext.h
@@ -15,7 +15,7 @@
 ** as extensions by SQLite should #include this file instead of 
 ** sqlite3.h.
 **
-** @(#) $Id: sqlite3ext.h,v 1.6 2007/06/02 17:48:56 rmsimpson Exp $
+** @(#) $Id: sqlite3ext.h,v 1.7 2007/06/19 22:43:24 rmsimpson Exp $
 */
 #ifndef _SQLITE3EXT_H_
 #define _SQLITE3EXT_H_
