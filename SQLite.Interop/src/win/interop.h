4abb065a1cafeea0a230febda7c95e6140c06b12
mistachkin <mistachkin>
2011-02-26 10:21:50 +0000

    Added interop project to streamlined build system.  Updated SQLite to version 3.7.5.  Fixed several warnings about unused local variables in extension-functions.c and interop.c.  Group the source files by their origin (for clarity).  Heavily revised the RC file for consistency and to centralize version information.  Unfortunately, VS2008 / VS2010 use entirely different schemas for their property files; therefore, two sets of property files are required.  Deleted the extraneous resource.h file.

---
+++ SQLite.Interop/src/win/interop.h
@@ -0,0 +1,12 @@
+/*
+ *
+ * interop.h -
+ *
+ * Written by Joe Mistachkin.
+ * Released to the public domain, use at your own risk!
+ *
+ */
+
+#ifndef INTEROP_VERSION
+#define INTEROP_VERSION       "1.0.67.0"
+#endif
