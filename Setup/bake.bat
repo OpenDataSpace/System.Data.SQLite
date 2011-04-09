42675944acee5555cc3cf7dd0eb418ccb5271eca
mistachkin <mistachkin>
2011-04-09 23:06:52 +0000

    Add missing diagnostic message for the PATH variable.

--- Setup/bake.bat
+++ Setup/bake.bat
@@ -20,6 +20,8 @@ IF NOT DEFINED _VECHO (SET _VECHO=REM)
 
 SET PATH=%ProgramFiles%\Inno Setup 5;%PATH%
 
+%_VECHO% Path = '%PATH%'
+
 %_ECHO% ISCC.exe SQLite.iss "/dAppId=%APPID%" "/dAppVersion=%VERSION%" "/dAppPublicKey=%PUBLICKEY%" "/dAppURL=%URL%" "/dIsNetFx2=%ISNETFX2%" "/dVcRuntime=%VCRUNTIME%" "/dAppPlatform=%PLATFORM%" "/dAppProcessor=%PROCESSOR%" "/dYear=%YEAR%"
 
 IF %ERRORLEVEL% NEQ 0 (
