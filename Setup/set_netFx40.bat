ca193b0e0e32b1e94db6dff5b30b491fe32f6034
mistachkin <mistachkin>
2012-01-25 06:46:25 +0000

    Add Setup/set_netFx40.bat to pre-configure the environment for building VS 2010 binary and setup packages.

---
+++ Setup/set_netFx40.bat
@@ -0,0 +1,38 @@
+@ECHO OFF
+
+::
+:: set_netFx40.bat --
+::
+:: Written by Joe Mistachkin.
+:: Released to the public domain, use at your own risk!
+::
+
+IF NOT DEFINED ISNETFX2 (
+  SET ISNETFX2=False
+)
+
+IF NOT DEFINED VCRUNTIME (
+  SET VCRUNTIME=2010_SP1
+)
+
+IF NOT DEFINED CONFIGURATION (
+  SET CONFIGURATION=Release
+)
+
+IF NOT DEFINED PLATFORM (
+  SET PLATFORM=Win32
+)
+
+IF NOT DEFINED PROCESSOR (
+  SET PROCESSOR=x86
+)
+
+IF NOT DEFINED YEAR (
+  SET YEAR=2010
+)
+
+IF NOT DEFINED FRAMEWORK (
+  SET FRAMEWORK=netFx40
+)
+
+:end_of_file
