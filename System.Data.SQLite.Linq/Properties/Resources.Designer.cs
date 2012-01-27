2ca68cec1cc3e27aaa63bd11ee4b415996bd40da
mistachkin <mistachkin>
2012-01-27 19:35:22 +0000

    Correct the resource name for the LINQ assembly.  Fix for ticket [fbebb30da9].

--- System.Data.SQLite.Linq/Properties/Resources.Designer.cs
+++ System.Data.SQLite.Linq/Properties/Resources.Designer.cs
@@ -46,7 +46,7 @@ namespace System.Data.SQLite.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Data.SQLite.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("System.Data.SQLite.Linq.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
