e7a4f70b28fc53af45d45e8a6876b9431e5c8192
mistachkin <mistachkin>
2013-02-12 10:51:01 +0000

    Adjust one #if directive in the SQLiteConnection class to explicitly exclude the .NET Compact Framework.

--- System.Data.SQLite/SQLiteConnection.cs
+++ System.Data.SQLite/SQLiteConnection.cs
@@ -488,7 +488,7 @@ namespace System.Data.SQLite
       SQLiteLog.Initialize();
 #endif
 
-#if SQLITE_STANDARD
+#if !PLATFORM_COMPACTFRAMEWORK && SQLITE_STANDARD
       //
       // NOTE: Check if the sqlite3_close_v2() native API should be available
       //       to use.  This must be done dynamically because the delegate set
