0e15f485206657cd4b3bd73672a2cfd942383f4e
mistachkin <mistachkin>
2013-04-30 00:29:01 +0000

    Update release version to 1.0.86.0.

--- System.Data.SQLite/AssemblyInfo.cs
+++ System.Data.SQLite/AssemblyInfo.cs
@@ -64,7 +64,7 @@ using System.Runtime.ConstrainedExecution;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.85.0")]
+[assembly: AssemblyVersion("1.0.86.0")]
 #if !PLATFORM_COMPACTFRAMEWORK
-[assembly: AssemblyFileVersion("1.0.85.0")]
+[assembly: AssemblyFileVersion("1.0.86.0")]
 #endif
