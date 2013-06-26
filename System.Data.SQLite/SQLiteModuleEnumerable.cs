c4af1814284220279510b32952f699bd84ca6773
mistachkin <mistachkin>
2013-06-26 18:17:47 +0000

    Add an example to the SQLiteModuleEnumerable class doc comments.

--- System.Data.SQLite/SQLiteModuleEnumerable.cs
+++ System.Data.SQLite/SQLiteModuleEnumerable.cs
@@ -217,7 +217,46 @@ namespace System.Data.SQLite
     /// <see cref="IEnumerable" /> object instance as a read-only virtual
     /// table.  It is not sealed and may be used as the base class for any
     /// user-defined virtual table class that wraps an
-    /// <see cref="IEnumerable" /> object instance.
+    /// <see cref="IEnumerable" /> object instance.  The following short
+    /// example shows it being used to treat an array of strings as a table
+    /// data source:
+    /// <code>
+    ///   public static class Sample
+    ///   {
+    ///     public static void Main()
+    ///     {
+    ///       using (SQLiteConnection connection = new SQLiteConnection(
+    ///           "Data Source=:memory:;"))
+    ///       {
+    ///         connection.Open();
+    ///
+    ///         connection.CreateModule(new SQLiteModuleEnumerable(
+    ///           "sampleModule", new string[] { "one", "two", "three" }));
+    ///
+    ///         using (SQLiteCommand command = connection.CreateCommand())
+    ///         {
+    ///           command.CommandText =
+    ///               "CREATE VIRTUAL TABLE t1 USING sampleModule;";
+    ///
+    ///           command.ExecuteNonQuery();
+    ///         }
+    ///
+    ///         using (SQLiteCommand command = connection.CreateCommand())
+    ///         {
+    ///           command.CommandText = "SELECT * FROM t1;";
+    ///
+    ///           using (SQLiteDataReader dataReader = command.ExecuteReader())
+    ///           {
+    ///             while (dataReader.Read())
+    ///               Console.WriteLine(dataReader[0].ToString());
+    ///           }
+    ///         }
+    ///
+    ///         connection.Close();
+    ///       }
+    ///     }
+    ///   }
+    /// </code>
     /// </summary>
     public class SQLiteModuleEnumerable : SQLiteModuleNoop /* NOT SEALED */
     {
@@ -305,7 +344,7 @@ namespace System.Data.SQLite
             )
         {
             SetCursorError(cursor, "already hit end of enumerator");
-            return SQLiteErrorCode.Error; 
+            return SQLiteErrorCode.Error;
         }
 
         ///////////////////////////////////////////////////////////////////////
