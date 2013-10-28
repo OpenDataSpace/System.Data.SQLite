2dd375b56198d7cb982ea8f2f5f13ae2b5f106f8
mistachkin <mistachkin>
2013-10-28 08:15:03 +0000

    Bump all version numbers to 1.0.90.0.  Update version history docs.

--- System.Data.SQLite/UnsafeNativeMethods.cs
+++ System.Data.SQLite/UnsafeNativeMethods.cs
@@ -673,7 +673,7 @@ namespace System.Data.SQLite
     //       System.Data.SQLite functionality (e.g. being able to bind
     //       parameters and handle column values of types Int64 and Double).
     //
-    internal const string SQLITE_DLL = "SQLite.Interop.089.dll";
+    internal const string SQLITE_DLL = "SQLite.Interop.090.dll";
 #elif SQLITE_STANDARD
     //
     // NOTE: Otherwise, if the standard SQLite library is enabled, use it.
