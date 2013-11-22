44465254b0437c8c5907dc700d67f1dfec0b1a34
mistachkin <mistachkin>
2013-11-22 03:50:35 +0000

    Add platform configuration batch files.

---
+++ Setup/set_ReleaseNativeOnly_WCE80_ARMV7TIEVM3730.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+::
+:: set_ReleaseNativeOnly_WCE80_ARMV7TIEVM3730.bat --
+::
+:: Written by Joe Mistachkin.
+:: Released to the public domain, use at your own risk!
+::
+
+CALL "%~dp0\set_ReleaseNativeOnly.bat"
---
+++ Setup/set_Release_WCE80_ARMV7TIEVM3730.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+::
+:: set_Release_WCE80_ARMV7TIEVM3730.bat --
+::
+:: Written by Joe Mistachkin.
+:: Released to the public domain, use at your own risk!
+::
+
+CALL "%~dp0\set_Release.bat"
