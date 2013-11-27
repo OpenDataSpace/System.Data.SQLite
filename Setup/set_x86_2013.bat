1b94abae31222b616d5d1cefa4993cae5411f5fe
mistachkin <mistachkin>
2013-11-27 00:05:34 +0000

    More build automation changes for Visual Studio 2013.

---
+++ Setup/set_x86_2013.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+::
+:: set_x86_2013.bat --
+::
+:: Written by Joe Mistachkin.
+:: Released to the public domain, use at your own risk!
+::
+
+SET ISNETFX2=False
+SET VCRUNTIME=2013_RTM
+SET PLATFORM=Win32
+SET PROCESSOR=x86
+SET YEAR=2013
