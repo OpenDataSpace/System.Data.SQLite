f0c20634abeb12a72e6bb2a62ebc4c464534ee24
mistachkin <mistachkin>
2013-11-27 05:28:03 +0000

    Updates to support the Visual Studio 2013 design-time components.

--- SQLite.Designer/SQLiteDataViewSupport.cs
+++ SQLite.Designer/SQLiteDataViewSupport.cs
@@ -1,7 +1,7 @@
 /********************************************************
  * ADO.NET 2.0 Data Provider for SQLite Version 3.X
  * Written by Robert Simpson (robert@blackcastlesoft.com)
- * 
+ *
  * Released to the public domain, use at your own risk!
  ********************************************************/
 
@@ -21,13 +21,16 @@ namespace SQLite.Designer
   internal sealed class SQLiteDataViewSupport : DataViewSupport
   {
     public SQLiteDataViewSupport()
-      : base(String.Format(CultureInfo.InvariantCulture, "SQLite.Designer.SQLiteDataViewSupport{0}", GetVSVersion()), typeof(SQLiteDataViewSupport).Assembly)
+      : base(String.Format(CultureInfo.InvariantCulture,
+          "SQLite.Designer.SQLiteDataViewSupport{0}", GetVSVersion()),
+          typeof(SQLiteDataViewSupport).Assembly)
     {
     }
 
     private static string GetVSVersion()
     {
-      switch (System.Diagnostics.FileVersionInfo.GetVersionInfo(Environment.GetCommandLineArgs()[0]).FileMajorPart)
+      switch (System.Diagnostics.FileVersionInfo.GetVersionInfo(
+          Environment.GetCommandLineArgs()[0]).FileMajorPart)
       {
         case 8:
           return "2005";
@@ -37,6 +40,8 @@ namespace SQLite.Designer
           return "2010";
         case 11:
           return "2012";
+        case 12:
+          return "2013";
         default:
           return null;
       }
