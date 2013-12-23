c73a29bc55ba962df06608d5af3e3b8bc82d8dfd
mistachkin <mistachkin>
2013-12-23 22:05:08 +0000

    More release automation fixes for Visual Studio 2013.

--- Setup/set_2013.bat
+++ Setup/set_2013.bat
@@ -13,4 +13,10 @@ SET NETFX40ONLY=
 SET NETFX45ONLY=
 SET NETFX451ONLY=1
 
+REM
+REM HACK: Evidently, using MSBuild with Visual Studio 2013 requires some
+REM       extra magic to make it recognize the "v120" platform toolset.
+REM
+SET BUILD_ARGS=/property:VisualStudioVersion=12.0
+
 VERIFY > NUL
